5cfc28ec15c322be43952cebab86bb49a7988435
Davengel5 <dav35eng@comunidad.unam.mx>
2025-05-12 21:29:02 -0600

    CCSC

--- start.sh
+++ start.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-# Crea directorio /data si no existe y asegura permisos
-mkdir -p /data && chmod 777 /data
-
-# Ejecuta creardb.php solo si no existe la BD
-[ ! -f /data/urls.db ] && php creardb.php
-
-#!/bin/bash
-php migrate.php && php -S 0.0.0.0:$PORT -t .
+php -S 0.0.0.0:$PORT -t .
